7df1858958670f30d6285b0cf46962622cf7fd1b
banerjri <banerjri@mail.uc.edu>
2023-06-18 23:15:41 -0500

    Project Completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        double Celsius;
+        double Farenheit;
+        String Trash;
+        boolean done = false;
+        do
+        {
+            System.out.print("Enter the temperature in Celsius :");
+            if(in.hasNextDouble())
+            {
+                Celsius = in.nextDouble();
+            in.nextLine();
+            Farenheit = 32 +((1.8) * Celsius);
+                System.out.println("The temperature " + Celsius + "C is" + Farenheit + "F");
+            done = true;
+             }
+    else
+        {
+            Trash = in.nextLine();
+            System.out.println("\nyour value entered was :" + Trash);
+            System.out.println("You have an invalid entry");
+        }
+        }while (!done);
     }
+
 }
